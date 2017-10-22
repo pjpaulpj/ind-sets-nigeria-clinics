767ccaf9aa21bc5c6a0df347b8a6d06c78e0d428
Akshat Goel <akshatgoel92@users.noreply.github.com>
2017-10-22 13:34:50 +0530

    Update full_data_mis.py

--- full_data_mis.py
+++ full_data_mis.py
@@ -11,7 +11,7 @@ project data-set. It also computes a sampling frame.
 For any questions related to this file, please contact 
 Akshat Goel at akshat.goel@idinsight.org.'''
 
-## Importing packages
+# Importing packages
 import numpy as np
 import pandas as pd	
 import networkx as nx
@@ -22,35 +22,36 @@ import geopy
 from geopy.distance import vincenty
 from shapely.geometry import Point				
 
-## Setting working directory 
+# Setting working directory 
 wd = '/Users/Akshat/Desktop/mis/'
 
 def create_distance_matrix(data, radius):
-	'''
-Creating distance matrix using Vincenty distance formula.'''
+	'''Creating distance matrix using Vincenty distance formula.'''
 	
-	## Creating co-ordinates as a two-element tuple and adding it to the data-set
-	data['coords'] = data[['properties/latitude', 'properties/longitude']].apply(tuple, axis=1)
-	## Creating distance matrix in kilometers (this does too much work because dist. mat. is symmetric)
+	# Creating co-ordinates as a two-element tuple and adding it to the data-set
+	data['coords'] = data[['properties/latitude', 
+			       'properties/longitude']].apply(tuple, axis=1)
+	# Creating distance matrix in kilometers (this does too much work because dist. mat. is symmetric)
 	distances = np.array([vincenty(x,y).km for x in data['coords'] for y in data['coords']])
-	## Reshaping data
+	# Reshaping data
 	square = distances.reshape(len(data),len(data))
-	## Indexing using pandas
+	# Indexing using pandas
 	square = DataFrame(square, index = data.id, columns = data.id)
-	### Return statement
+	# Return statement
 	return(square)
 
-## Creates adjacency matrix
+# Creates adjacency matrix
 def create_adj_matrix(data, distances, radius):	
 ''' 
 Creating adjacency matrix representation of a graph 
 given an edge-list representation as a pandas data-frame. '''
 	
-	## Creating adjacency matrix 
-	adj_matrix = DataFrame(np.where(distances > radius, 1,0), index = data.id, columns = data.id)
+	# Creating adjacency matrix 
+	adj_matrix = DataFrame(np.where(distances > radius, 1,0), 
+			       index = data.id, columns = data.id)
 	return(adj_matrix)
 
-## Creates NetworkX graph from adjacency matrix
+# Creates NetworkX graph from adjacency matrix
 def create_graph(adjacency_matrix):
 ''' 
 This function takes an adjacency matrix and uses NetworkX to create a graph.
@@ -63,7 +64,7 @@ Python indexing begins with 0. '''
 	gr.add_edges_from(edges)
 	return(gr)
 	
-## Implement code
+# Implement code
 def deploy_graph(data = "health facilties ehealth africa.csv", rad = 20):
 '''
 This function deploys the code above on our data-set. It works as follows:
@@ -78,28 +79,30 @@ This function deploys the code above on our data-set. It works as follows:
 	graph = create_graph(adj_matrix)
 	nx.write_gpickle(graph, wd + "test_" + str(rad) + ".gpickle")
 	
-## Method calls
+# Method calls
 deploy_graph()
 
-
+def max_set_ec2_deploy(graph_data): 
 '''
-This code is run on an Amazon EC2 instance.
+This code should be run on an Amazon EC2 instance or other cloud
+computing platform. It actually runs max. ind. sets on the graph 
+file created above.'''
 
-## Storing cardinality of largest set
-cliques = approximation.clique_removal(graph)[1]
-## Storing biggest cliques
-biggest = max([len(cliques[x]) for x in range(len(cliques))])
-## Adding 1 to get clinic number		 
-cliques = np.array([list(x) for x in cliques if len(x) == biggest])	
-## Adding location identifier 'K' 
-cliques = cliques[:][:]+ 1
-## Storing filepath											
-cliques = DataFrame(np.char.mod('K'+ '%04d', cliques))
-## Writing to Excel file				 
-filepath = wd + 'solutions.xlsx'
-## Exporting								 	 
-cliques.T.to_excel(filepath, index=False)							
-'''
+	# Storing cardinality of largest set
+	cliques = approximation.clique_removal(graph)[1]
+	# Storing biggest cliques
+	biggest = max([len(cliques[x]) for x in range(len(cliques))])
+	# Adding 1 to get clinic number		 
+	cliques = np.array([list(x) for x in cliques if len(x) == biggest])	
+	# Adding location identifier 'K' 
+	cliques = cliques[:][:]+ 1
+	# Storing filepath											
+	cliques = DataFrame(np.char.mod('K'+ '%04d', cliques))
+	# Writing to Excel file				 
+	filepath = wd + 'solutions.xlsx'
+	# Exporting								 	 
+	cliques.T.to_excel(filepath, index=False)							
+
 
 
 
